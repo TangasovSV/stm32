06dd3dba35c7ead74da414e191b3a55a4837e899
TangasovSV <91424110+TangasovSV@users.noreply.github.com>
2021-09-29 21:39:41 +0300

    Update delay.h

--- delay.h
+++ delay.h
@@ -6,7 +6,11 @@ class DELAY {
 public:
 	static void INIT();
 	static void MS(uint16_t value);
-	static void US(uint16_t value);	
+	inline static void US(uint_fast16_t value) {
+		SysTick->VAL = 0;
+		SysTick->LOAD = (SystemCoreClock / 1000000 /*-1 must be here. But "minus" operation is so long...*/) * value;    // 1MHz Frequency
+		while(!(SysTick->CTRL & 0x10000)) __NOP();
+	}
 	static void TimeOut(uint16_t value);
 	static int GetTick();
 };
